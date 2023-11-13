9742420536fc6a2a8d214fae6741dd3c589e8f3b
Maikon Douglas <maikon.douglas@vindi.com.br>
2023-11-13 01:00:41 -0300

    feat: add list for opening handler

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/maikondouglas/gopportunities/schemas"
 )
 
 func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, "Error listing openings")
+	}
+
+	sendSuccess(context, "list-openings", openings)
 }
